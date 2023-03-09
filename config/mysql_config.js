52f0baa507276730d2666a355c7583751ae8508b
Morning <morning@1-14.local>
2023-03-09 13:07:36 +0800

    updated

---
+++ config/mysql_config.js
@@ -0,0 +1,11 @@
+//IMPORTING SQL DATA FROM "ECOMMERCEDB"
+import mysql from "mysql2"
+
+export const pool = mysql
+.createPool({
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "morning",
+})
+.promise();
